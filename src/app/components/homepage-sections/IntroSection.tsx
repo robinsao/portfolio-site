53dbbe2e49db06d98ffc11ee5290b95d0317c244
robinsao <s.robin.sao@gmail.com>
2025-09-05 10:32:51 +1000

    redesigned and reworded intro section

--- src/app/components/homepage-sections/IntroSection.tsx
+++ src/app/components/homepage-sections/IntroSection.tsx
@@ -8,17 +8,20 @@ export default function IntroSection() {
     >
       {/* Headline & description */}
       <div className="max-w-xl">
-        <h1 className="text-4xl md:text-5xl text-center md:text-left font-extrabold mb-6 leading-tight text-gray-900 dark:text-white">
-          Turning Vision Into
-          <br />
-          Reality With Code
-          <br />
-          And Design.
+        <h1 className="text-2xl sm:text-3xl md:text-5xl text-center md:text-left font-extrabold mb-6 leading-tight text-gray-900 dark:text-white">
+          <span className="hidden sm:inline-block md:hidden lg:inline-block transform scale-x-150">
+            - &nbsp;
+          </span>
+          Hello, I&apos;m Robin
+          <span className="hidden sm:inline-block md:hidden lg:inline-block transform scale-x-150">
+            &nbsp; -
+          </span>
         </h1>
         <p className="text-base md:text-lg text-gray-700 dark:text-gray-300 mb-8 text-center md:text-justify">
-          As a skilled full-stack developer, I am dedicated to turning ideas
-          into innovative web applications. Explore my latest projects and
-          articles, showcasing my expertise in React.js and web development.
+          As a developer with strong foundational skills in web development and
+          cloud computing, I love to work on impactful projects that solves
+          real-world problems. Explore my projects and blogs below, to gain
+          insights into my skills and the tech stack I work with.
         </p>
       </div>
       {/* Profile */}
